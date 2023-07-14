1d88e22f62a4c7745e061b92117d4a4801b92a6e
ThiagoAug <77154719+ThiagoAug@users.noreply.github.com>
2023-07-14 11:38:47 -0400

    Removing ImageService
    Removing ImageService

--- src/main/java/com/br/regUser/config/AwsConfig.java
+++ src/main/java/com/br/regUser/config/AwsConfig.java
@@ -1,16 +1,18 @@
 package com.br.regUser.config;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class AwsConfig {
+    @Value("${amazon.user.accesskey}")
+    private String accessKey;
+    @Value("${amazon.user.secretkey}")
+    private String secretKey;
     @PostConstruct
     public void configureAwsCredentials() {
-        // Configurar as chaves de acesso da AWS
-        String accessKey = "AKIASB4FIJ7NQJJ62D7X";
-        String secretKey = "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";
 
         // Definir as propriedades do sistema
         System.setProperty("aws.accessKeyId", accessKey);
